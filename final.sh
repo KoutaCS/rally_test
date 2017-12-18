f2af3d767dc4c5946be01abf3a208b3c56bd5309
KoutaCS <chits.boorana@hotmail.com>
2017-12-18 14:49:18 +0700

    setup staging

--- final.sh
+++ final.sh
@@ -1,17 +1,10 @@
-echo -n $(date +(%H:%M:%S));echo " OpenStack Performance testing: Start"
-
-# get non-root username
-user=`who`
-echo $user |cut -d ' ' -f 1 >> user.log
-USER=$(cat user.log)
-rm -rf user.log
-TIMEDATE=$(date +%m-%d-%y)
+#!/bin/bash
 
 # make output folder
 mkdir ~/rally-result
 
 # activate rally and use deployment
-. /home/"$USER"/rally/bin/activate
+. /openstack/venvs/rally-16.0.5/bin/activate
 rally deployment use rally-deployment
 cd task
 
@@ -20,7 +13,3 @@ rally task start service_task.yaml --task-args-file testing_arguments.yaml
 rally task report --out ~/rally-result/service_test.html --open 
 rally task start launch_instance.yaml --task-args-file launch_arguments.yaml
 rally task report --out ~/rally-result/launch_test.html --open
-
-# move report to home for "scp"
-cp -rf ~/rally-result /home/"$USER"/
-echo -n $(date +(%H:%M:%S));echo " OpenStack Performance testing: Finish"
--- setup.sh
+++ setup.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-echo -n $(date +(%H:%M:%S));echo " Setting environment: Setting"
-
-user=`who`
-echo $user |cut -d ' ' -f 1 >> user.log
-USER=$(cat user.log)
-rm -rf user.log
-
-. /home/"$USER"/rally/bin/activate
+. /openstack/venvs/rally-16.0.5/bin/activate
 
 . ~/openrc
 rally deployment create --fromenv --name rally-deployment
@@ -15,7 +8,5 @@ rally deployment create --fromenv --name rally-deployment
 mkdir ~/.rally
 cp -R /home/"$USER"/rally/src/rally-jobs/extra ~/.rally
 git clone https://github.com/KoutaCS/nipa.cloud_scenario ~/.rally/plugins
-
-echo -n $(date +(%H:%M:%S));echo " Setting environment: Finish"
 
 . final.sh
