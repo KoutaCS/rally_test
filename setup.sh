d7f5ffa4ce6ba87b27b32305cb6c5fca4acd3882
KoutaCS <chits.boorana@hotmail.com>
2017-12-18 17:02:45 +0700

    testing launch instance script

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@
 rally deployment create --fromenv --name rally-deployment
 
 mkdir ~/.rally
-cp -R /home/"$USER"/rally/src/rally-jobs/extra ~/.rally
 git clone https://github.com/KoutaCS/nipa.cloud_scenario ~/.rally/plugins
 
 . final.sh
